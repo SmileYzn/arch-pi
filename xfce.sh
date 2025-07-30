ef32c7ac109c6a8fcc4675b9edfe9acec957d8e2
Cleverson <SmileYzn@users.noreply.github.com>
2025-07-30 14:52:40 -0300

    Update xfce.sh

--- xfce.sh
+++ xfce.sh
@@ -154,7 +154,7 @@ executar()
       sudo pacman -S --needed --noconfirm drawing peek gthumb
     ;;
     13)
-      sudo pacman -S --needed --noconfirm catfish dconf-editor gnome-online-accounts gparted lightdm-gtk-greeter-settings mate-calc mpv mugshot seahorse zeitgeist
+      sudo pacman -S --needed --noconfirm catfish dconf-editor gigolo gnome-online-accounts gparted lightdm-gtk-greeter-settings mate-calc mpv mugshot orage seahorse zeitgeist
     ;;
     14)
       # Instalar o pacote YAY no modo binário
